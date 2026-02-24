bec85557c267f635f93a6addff9cd4b2a97d6098
m1v8a <245783116+m1v8a@users.noreply.github.com>
2026-02-24 21:23:03 +0800

    recursion: add solutions to factorial and contains

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,9 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) return;
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
 module.exports = factorial;
+
--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,20 @@
-const contains = function() {
-  
+const contains = function (object, target) {
+  if (Array.isArray(object) || typeof object !== "object" || object === null) {
+    if (Number.isNaN(target)) {
+      return Number.isNaN(object);
+    }
+    return object === target;
+  }
+
+  let isTargetPresent = false;
+  for (const key in object) {
+    isTargetPresent = contains(object[key], target);
+    if (isTargetPresent) break;
+  }
+  return isTargetPresent;
 };
-  
+
+contains({ huh: "What" });
+
 // Do not edit below this line
 module.exports = contains;
